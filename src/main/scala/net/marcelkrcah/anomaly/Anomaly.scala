7a5a9f74b16c5be047bc1ceb0f31c2ce92c6626e
Marcel Krcah <marcel.krcah+git@gmail.com>
2014-07-12 21:06:35 +0200

    compute ngrams

--- src/main/scala/net/marcelkrcah/anomaly/Anomaly.scala
+++ src/main/scala/net/marcelkrcah/anomaly/Anomaly.scala
@@ -1,9 +1,21 @@
 package net.marcelkrcah.anomaly
 
+import scala.io.Source
+
 object Anomaly extends App{
 
-   def toBigram(s:String) = 1
+  val names = {
+    val rawNames = Source.fromFile("data/names.txt").getLines()
+    def normalize(rawName:String) = rawName.toLowerCase.trim
+    rawNames.map(normalize)
+  }
 
-   println("Hello world")
+  println("aabbaa".sliding(2).toList.groupBy(identity).mapValues(_.length))
+
+   def toNgram(s:String, n:Int) = {
+     s.sliding(n).toList.groupBy(identity).mapValues(_.length)
+   }
+
+//   println(names.mkString("\n"))
 
  }
--- src/test/scala/net/marcelkrcah/anomaly/AnomalySpec.scala
+++ src/test/scala/net/marcelkrcah/anomaly/AnomalySpec.scala
@@ -2,9 +2,15 @@ package net.marcelkrcah.anomaly
 
 import org.scalatest._
 
-class AnomalySpec extends FlatSpec with Matchers {
+class AnomalySpec extends FunSpec with Matchers {
 
-  "An app" should "should have working specs" in {
-      Anomaly.toBigram("aa") should be (1)
+  describe("Anomaly detector") {
+    it("should compute bigrams") {
+      Anomaly.toNgram("aabaa", 2) should be (Map("aa"->2, "ab"->1, "ba"->1))
+    }
+    it("should compute 3-grams") {
+      Anomaly.toNgram("aaabaaa", 3) should be (Map("aaa"->2, "aab"->1, "aba"->1, "baa"->1))
+    }
   }
+
 }
